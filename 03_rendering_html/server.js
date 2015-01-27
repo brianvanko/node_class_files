aacf68b87e134f90d05ef0d43870c8df38543608
Brian Vanko <brianvanko@gmail.com>
2015-01-26 18:53:23 -0800

    added handlebars views and template to basic greeting app

---
+++ 03_rendering_html/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 9000;
+var exphbs = require('express-handlebars');
+
+app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'main.hbs'}));
+app.set('view engine', 'hbs');
+//app.set('views', __dirname + '/other_folder') - overrides current views folder
+
+
+app.get('/', function (req, res) {
+	console.log('welcome to our site');
+})
+
+app.get('/:name', function (req, res) {
+	res.render('greeting', { title: 'Simple Greeting App', name: req.params.name })
+})
+
+app.listen(port);
+console.log('listening on port ' + port)
