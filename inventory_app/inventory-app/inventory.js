f0807e90c0f358dab021d8356fd3ae34a366da0c
Brian Vanko <brianvanko@gmail.com>
2015-01-26 19:41:36 -0800

    fixed a typo.  items not item

--- inventory_app/inventory-app/inventory.js
+++ inventory_app/inventory-app/inventory.js
@@ -1,5 +1,5 @@
 //var items = [];
-var item = [{name: 'Couch', description: 'this is comfortable'}, {name: 'Laptop', description: 'beep boop bleep!'}];
+var items = [{name: 'Couch', description: 'this is comfortable'}, {name: 'Laptop', description: 'beep boop bleep!'}];
 
 exports.list = function (req, res) {
 	res.render('index', {items: items});
